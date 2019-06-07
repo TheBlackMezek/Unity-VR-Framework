19d5be549f7e9a54edd05d40082d82f789bbc025
unknown <s179069@students.aie.edu.au>
2019-06-07 16:53:43 -0700

    Finish up most of input system

---
+++ Assets/Scripts/DeviceFollower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+//This is to be placed on the objects which represent the hands or head to provide highest-accuracy physical device data during gameplay
+
+public class DeviceFollower : MonoBehaviour
+{
+
+    [SerializeField]
+    private DeviceEnum deviceEnum;
+
+    private InputDevice device;
+
+
+
+    private void Awake()
+    {
+        PreUpdater.PreUpdate += PreUpdate;
+
+        GetDevice();
+    }
+
+    private void PreUpdate()
+    {
+        Vector3 pos;
+        Quaternion rot;
+
+        device.TryGetFeatureValue(CommonUsages.devicePosition, out pos);
+        device.TryGetFeatureValue(CommonUsages.deviceRotation, out rot);
+
+        transform.localPosition = pos;
+        transform.localRotation = rot;
+    }
+
+    private void GetDevice()
+    {
+        switch (deviceEnum)
+        {
+            case DeviceEnum.HEAD:
+                device = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+                break;
+            case DeviceEnum.LEFT_HAND:
+                device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+                break;
+            case DeviceEnum.RIGHT_HAND:
+                device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+                break;
+        }
+        //The device might not have been recognized yet so this check is necessary
+        if (device.name == "")
+            Invoke("GetDevice", 0.1f);
+    }
+
+}
---
+++ Assets/Scripts/InputManagerTester.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManagerTester : MonoBehaviour
+{
+
+    [Header("Colors")]
+
+    [SerializeField] private Color neuralColor;
+    [SerializeField] private Color downColor;
+    [SerializeField] private Color heldColor;
+    [SerializeField] private Color upColor;
+
+    [Header("Left")]
+
+    [SerializeField] private MeshRenderer leftTrigger;
+    [SerializeField] private MeshRenderer leftGrip;
+    [SerializeField] private MeshRenderer leftPadTouch;
+    [SerializeField] private MeshRenderer leftPadClick;
+    [SerializeField] private MeshRenderer leftMenu;
+
+    [Header("Right")]
+
+    [SerializeField] private MeshRenderer rightTrigger;
+    [SerializeField] private MeshRenderer rightGrip;
+    [SerializeField] private MeshRenderer rightPadTouch;
+    [SerializeField] private MeshRenderer rightPadClick;
+    [SerializeField] private MeshRenderer rightMenu;
+
+
+
+    private void Update()
+    {
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+
+        #region Left
+        if (VRInputManager.LeftTriggerDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.LeftTriggerUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.LeftTrigger)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        leftTrigger.SetPropertyBlock(block);
+
+        if (VRInputManager.LeftGripDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.LeftGripUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.LeftGrip)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        leftGrip.SetPropertyBlock(block);
+
+        if (VRInputManager.LeftPadTouchDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.LeftPadTouchUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.LeftPadTouch)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        leftPadTouch.SetPropertyBlock(block);
+
+        if (VRInputManager.LeftPadClickDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.LeftPadClickUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.LeftPadClick)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        leftPadClick.SetPropertyBlock(block);
+
+        if (VRInputManager.LeftMenuDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.LeftMenuUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.LeftMenu)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        leftMenu.SetPropertyBlock(block);
+        #endregion
+
+        #region Right
+        if (VRInputManager.RightTriggerDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.RightTriggerUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.RightTrigger)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        rightTrigger.SetPropertyBlock(block);
+
+        if (VRInputManager.RightGripDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.RightGripUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.RightGrip)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        rightGrip.SetPropertyBlock(block);
+
+        if (VRInputManager.RightPadTouchDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.RightPadTouchUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.RightPadTouch)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        rightPadTouch.SetPropertyBlock(block);
+
+        if (VRInputManager.RightPadClickDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.RightPadClickUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.RightPadClick)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        rightPadClick.SetPropertyBlock(block);
+
+        if (VRInputManager.RightMenuDown)
+            block.SetColor("_Color", downColor);
+        else if (VRInputManager.RightMenuUp)
+            block.SetColor("_Color", upColor);
+        else if (VRInputManager.RightMenu)
+            block.SetColor("_Color", heldColor);
+        else
+            block.SetColor("_Color", neuralColor);
+
+        rightMenu.SetPropertyBlock(block);
+        #endregion
+    }
+
+}
