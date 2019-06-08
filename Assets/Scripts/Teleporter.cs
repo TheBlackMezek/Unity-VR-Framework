9e5dad6efd12c7966533680b7dff9b7c2d8da5ac
unknown <s179069@students.aie.edu.au>
2019-06-07 20:27:04 -0700

    Add base teleport script and Handedness enum

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Teleporter : MonoBehaviour
+{
+
+    [Header("Settings")]
+
+    [SerializeField] private Handedness hand;
+    [SerializeField] private float maxDist;
+    [SerializeField] private LayerMask teleportMask;
+
+    [Header("Links")]
+
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform playerObject;
+
+    private bool validTeleport;
+
+
+
+    private void Start()
+    {
+        if(hand == Handedness.LEFT)
+        {
+            VRInputManager.OnLeftPadClickDown.AddListener(SelectionStart);
+            VRInputManager.OnLeftPadClick.AddListener(SelectionUpdate);
+            VRInputManager.OnLeftPadClickUp.AddListener(Teleport);
+        }
+        else
+        {
+            VRInputManager.OnRightPadClickDown.AddListener(SelectionStart);
+            VRInputManager.OnRightPadClick.AddListener(SelectionUpdate);
+            VRInputManager.OnRightPadClickUp.AddListener(Teleport);
+        }
+
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 2;
+    }
+
+    private void SelectionStart()
+    {
+        lineRenderer.enabled = true;
+    }
+
+    private void SelectionUpdate()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDist, teleportMask))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+            validTeleport = true;
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.position + transform.forward * maxDist);
+            validTeleport = false;
+        }
+    }
+
+    private void Teleport()
+    {
+        lineRenderer.enabled = false;
+
+        if(validTeleport)
+            playerObject.position = lineRenderer.GetPosition(1);
+    }
+
+}
