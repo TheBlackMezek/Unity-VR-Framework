f97004a8e4b261f37997065267680f838fae5ad2
unknown <s179069@students.aie.edu.au>
2020-02-18 15:07:52 -0800

    Enable VR for project by default

---
+++ Assets/Unity VR Framework/Scripts/AutoSettingsConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.XR;
+
+[InitializeOnLoad]
+public class AutoSettingsConfig
+{
+
+    private const string PACKAGE_MANIFEST_PATH = "Packages/manifest.json";
+
+    static AutoSettingsConfig()
+    {
+        PlayerSettings.virtualRealitySupported = true;
+    }
+
+}
